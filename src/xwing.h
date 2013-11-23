912a0dfe405b9e714999197ae14629bcd468166d
jmarsico <jmarsico@gmail.com>
2013-11-23 18:47:11 -0500

    small changes to xwing.cpp

--- src/xwing.h
+++ src/xwing.h
@@ -27,28 +27,3 @@ public:
 };
 
 #endif
-
-class testApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		double px, py, pz;
-
-		ofBoxPrimitive pbody;
-		ofCylinderPrimitive pcyl[8];
-		ofBoxPrimitive pwings[4];
-
-		vector<ofMeshFace> triangles;
-};
