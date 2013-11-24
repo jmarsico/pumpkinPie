d5010ecde0c16c96ba328558245a2a2cc0eb1cbd
Atom <dajung@andrew.cmu.edu>
2013-11-24 12:18:58 -0500

    quick save

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,52 @@
 
 #include "Game.h"
 
-class Game{
-public:
+void Game::initialize(void){
+    /*initialEnemyPositions = new int[2*numEnemies];
+    enemies = new Participant[numEnemies]
+    for (int i = 0; i < numEnemies; i++){
+        enemies
+    }
+    */
     
-private:
+    player.initialize();
+    world = new World();
+    world.initialize();
+    isGameOn = true;
     
-};
+}
 
+void Game::update(void){
+    // TODO: update missiles
+    world.advance();
+    // check for a collision between player and an obstacle
+    
+}
+
+void Game::draw(void){
+    // TODO: draw enemies
+    Obstacle obstacles[] = world.getVisibleObstacles();
+    
+    /*
+    EnemyOne enemyOnez[] = world.getVisibleEnemyOne();
+    EnemyTwo enemyTwoz[] = world.getVisibleEnemyTwo();
+    EnemyThree enemyThreez[] = world.getVisibleEnemyThree();
+    */
+    
+    
+}
+
+void Game::endGame(void){
+    // TODO: cleanup world, enemies, initialEnemyPositions
+    isGameOn = false;
+    world.cleanup();
+    
+}
+
+int Game::moveParticipant(Direction d, Participant p){
+    
+}
+
+void Game::playerShoot(void){
+    
+}
--- src/Game.h
+++ src/Game.h
@@ -10,5 +10,45 @@
 #define __pumpkinPie__Game__
 
 #include <iostream>
+#include "Participant.h"
+#include "ofMain.h"
+
+enum Direction{
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+};
+
+class Game{
+public:
+    // initializes the fields (mui importante) and starts game
+    void initialize(void);
+    
+    // updates by advancing position of enemies, missiles, and the displayed section
+    void update(void);
+    
+    // draws the player, enemies, obstacles, and everything else that needs to be seen
+    void draw(void);
+    
+    // ends game, sets flag to false, then cleans up memory
+    void endGame(void);
+    
+    // used primarily for player movement
+    int moveParticipant(Direction d Participant p);
+    void playerShoot(void);
+    
+private:
+    Player player;
+    
+    World world;
+    bool isGameOn;
+
+    // enemy variables
+    //static int numEnemies = 1;
+    //int * initialEnemyPositions;
+    //Participant * enemies;
+    //EnemyBoss boss;
+};
 
 #endif /* defined(__pumpkinPie__Game__) */
